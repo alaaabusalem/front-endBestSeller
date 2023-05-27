fec5bba9ab57742bd59436fa5e423483e205fcbf
anasgharaibeh <anas.gharaibeh1@yahoo.com>
2023-05-27 21:01:29 +0000

    home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,46 @@
-export default function Navbar (){
+import { NavLink } from "react-router-dom";
+import logo from '../assets/logo.svg';
 
-	return (
-		<>
-    <ul><li>Navbar</li></ul>
-		</>
-	)
+export default function Navbar() {
+  const logoStyle = {
+    width: '130px',
+    height: 'auto',
+    borderRadius: '40%',
+  };
+
+  const navbarStyle = {
+    height: '100px', // Set the desired height
+  };
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary" style={navbarStyle}>
+        <div className="container-fluid">
+          <img src={logo} alt="book logo" style={logoStyle} />
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link active" to="/">Home
+                  <span className="visually-hidden">(current)</span>
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/reading">What I'm Reading Now</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/wishlist">Wish List</NavLink>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-sm-2" type="search" placeholder="Search" />
+              <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
