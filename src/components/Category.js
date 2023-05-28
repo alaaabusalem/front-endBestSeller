4507ffa554d2772a0b779bcad5e672e98f97b4f4
YamanAyoun <yamanayoun0@gmail.com>
2023-05-28 10:04:31 +0000

    add card

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,37 @@
-export default function Category() {
-	return(
-		<>
-		<h1>Category page</h1>
-		</>
-	)
-	
+import axios from "axios";
+import { useEffect, useState } from 'react';
+function Category({category}) {
+    const [Categorys, setCategorys] = useState([]);
+    const getCategorys = () => {
+        const serverURL = `${process.env.REACT_APP_ServerURL}/category-items?list_name_encoded=${category}`;
+        axios.get(serverURL)
+            .then(response => {
+                console.log(response.data)
+                setCategorys(response.data)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    useEffect(() => {
+        getCategorys()
+    }, [])
+    return(
+        <>
+         {Categorys.map(item=>{
+      return (
+        <div className="card border-danger mb-3" style={{maxWidth:"20rem"}}>
+        <div className="card-header">{item.author}</div>
+        <img src={item.book_image} alt="about"/>
+        <div className="card-body">
+          <h4 className="card-title">{item.title}</h4>
+          <p className="card-text">{item.description}</p>
+          <p className="card-text">{item.buy_links[0].url}</p>
+        </div>
+        </div>
+        );
+     })}
+        </>
+    )
 }
+export default Category;
