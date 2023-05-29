a63481b4a16cbf600749b549eea45993b33863e4
YamanAyoun <yamanayoun0@gmail.com>
2023-05-29 12:28:19 +0000

    solve problem

--- src/components/About.js
+++ src/components/About.js
@@ -4,46 +4,46 @@ function About() {
 
     return (
         <>
-            <h1><strong>Book Library</strong></h1> <br />
-            <p>Welcome to <b>(Book Library)</b> - your gateway to the world of reading!
+            <h1 id="h1"><strong>Book Library</strong></h1> <br />
+            <p id ="p">Welcome to <b>(Book Library)</b> - your gateway to the world of reading!
             </p>
 
-            <p>At (Book library), we believe books have the power to transport us to different worlds,
+            <p id ="p">At (Book library), we believe books have the power to transport us to different worlds,
                 spark our imaginations, and expand our knowledge.
                 We are an online books site dedicated to connecting book lovers with a wide range of literary treasures from all different genres,
                 eras and categories.
             </p>
 
-            <p>Our mission is to provide a virtual haven for book lovers,
+            <p id ="p">Our mission is to provide a virtual haven for book lovers,
                 where they can explore, discover and immerse themselves in the magic of words.
                 With a library of digital books, we strive to cater to all reading preferences, from exciting books like food, sports,
                 education, games or health books etc...
             </p>
 
-            <p>Discovering your next literary adventure is made easy on (Book Library).
+            <p id ="p">Discovering your next literary adventure is made easy on (Book Library).
                 The site's features help you add your favorite books to the Wish List page, so that you can easily access your favorite book.
                 The site also contains a reading now page that helps with reading in terms of putting quotes that you like in each book, or you can put the page number you reached in each book.
                 And after completing the book, you can delete it from the list and start in other books.</p>
 
-            <p>
+            <p id ="p">
                 Embracing the digital age, (Book Library) provides a seamless experience on various platforms.
                 Our easy-to-use interface allows you to access your favorite books anytime and anywhere from your desktop, tablet or smartphone.
                 Through the site, you can find the books you want to buy from the following sites (Amazon, Apple Books).
             </p>
 
-            <p>You can now join us today and immerse yourself in the boundless world of books.
+            <p id="p">You can now join us today and immerse yourself in the boundless world of books.
                 Let (Book Library) be your trusted companion as you unravel stories, expand your horizons, and embark on endless adventures.
             </p>
 
-            <p className="text-warning"><b>Who we are ?</b></p>
-            <p>
+            <p className="text-warning" id ="p"><b>Who we are ?</b></p>
+            <p id ="p">
                 We are a team of four programmers who are able to build websites that contain beautiful and creative ideas.
             </p>
-            <h4 className="text-warning">Team members:</h4>
-            <h6>Yaman Ayoun</h6>
-            <h6>Ammar Tarawneh</h6>
-            <h6>Ala' Abusalem</h6>
-            <h6>Anas Al Shammah</h6>
+            <h4 className="text-warning" id="h4">Team members:</h4>
+            <h6 id="h6">Yaman Ayoun</h6>
+            <h6 id="h6">Ammar Tarawneh</h6>
+            <h6 id="h6">Ala' Abusalem</h6>
+            <h6 id="h6">Anas Al Shammah</h6>
         </>
     )
 }
